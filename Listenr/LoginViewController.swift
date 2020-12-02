72bf30367feab856ce2e06d70a88c1cc9acc6d8d
kcolson8 <kelliepanda@gmail.com>
2020-12-01 17:45:38 -0800

    error handling of user login

--- Listenr/LoginViewController.swift
+++ Listenr/LoginViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class LoginViewController: UIViewController {
 
@@ -13,11 +14,56 @@ class LoginViewController: UIViewController {
     @IBOutlet var passwordTextField: UITextField!
     
     
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    @IBAction func loginButtonPressed(_ sender: UIButton) {
+        if emailTextField.text == "" {
+            let alertController = UIAlertController(title: "Missing Email", message: "An email for your account has not been entered", preferredStyle: .alert)
+            alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            present(alertController, animated: true, completion: nil)
+        } else if passwordTextField.text == "" {
+            let alertController = UIAlertController(title: "Missing Password", message: "A password for your account has not been entered", preferredStyle: .alert)
+            alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+            present(alertController, animated: true, completion: nil)
+        }
+        
+        if let email = emailTextField.text, let password = passwordTextField.text {
+            Auth.auth().signIn(withEmail: email, password: password) { [weak self] authResult, error in
+                if let resultError = error {
+                    print(resultError)
+                    if let errCode = resultError as NSError? {
+                        switch AuthErrorCode(rawValue: errCode.code) {
+                        case .wrongPassword:
+                            print("wrong password")
+                            let alertController = UIAlertController(title: "Wrong Password", message: "The password entered does not match the account with the given email", preferredStyle: .alert)
+                            alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                            self!.present(alertController, animated: true, completion: nil)
+                        case .invalidEmail:
+                            print("invalid email")
+                            let alertController = UIAlertController(title: "Invalid Email", message: "The email entered is not registered with an account", preferredStyle: .alert)
+                            alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                            self!.present(alertController, animated: true, completion: nil)
+                        default:
+                            print("other error")
+                            let alertController = UIAlertController(title: "Login Error", message: "An error occured logging you in. Make sure the credentials you have entered are correct.", preferredStyle: .alert)
+                            alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: nil))
+                            self!.present(alertController, animated: true, completion: nil)
+                        }
+                    }
+                } else {
+                    print("successfully logged in user")
+                    print(authResult!)
+                    // should segue to next screen
+                    self!.performSegue(withIdentifier: "loginSegue", sender: self)
+
+                }
+            }
+        }
+    }
+    
+    
 
 
 }
